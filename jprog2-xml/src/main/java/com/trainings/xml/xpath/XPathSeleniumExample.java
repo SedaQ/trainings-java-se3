90b29158fad97e4c2fbab5941a45eced2d06dc63
SedaQ <pavelseda@email.cz>
2018-11-10 15:21:28 +0100

    Selenium example added.

--- jprog2-xml/src/main/java/com/trainings/xml/xpath/XPathSeleniumExample.java
+++ jprog2-xml/src/main/java/com/trainings/xml/xpath/XPathSeleniumExample.java
@@ -1,7 +1,43 @@
 package com.trainings.xml.xpath;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.regex.Pattern;
+
 /**
  * @author Pavel Seda
  */
 public class XPathSeleniumExample {
+
+    public static void main(String[] args) {
+        XPathSeleniumExample xPathExample = new XPathSeleniumExample();
+        xPathExample.logInFacebook();
+    }
+
+    private void logInFacebook() {
+        // https://chromedriver.storage.googleapis.com/index.html?path=2.42/ - driver for Selenium
+
+        System.setProperty("webdriver.chrome.driver", "./jprog2-xml/src/main/resources/chrome-driver/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://facebook.com");
+
+        WebElement username = driver.findElement(By.id("email"));
+        username.sendKeys("java-trainings@email.cz");
+
+        WebElement password = driver.findElement(By.id("pass"));
+        password.sendKeys("JavaTrainings");
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
+
+        loginButton.click();
+
+        //WebDriverWait wait = new WebDriverWait(driver, 180);
+        //wait.until(ExpectedConditions.textMatches(By.xpath("//span[@class='routeDistance']"), Pattern.compile("\\w+")));
+    }
 }
