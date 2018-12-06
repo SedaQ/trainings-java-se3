c5d399bb5eaf602a00da1eeaf24c5bf0761aec2d
Pavel Seda <pavelseda@email.cz>
2018-12-06 15:51:09 +0100

    Read first few lines.

--- jprog2-nio2/src/main/java/com/trainings/nio/interactingwithfiles/csv/ReadCsvFile.java
+++ jprog2-nio2/src/main/java/com/trainings/nio/interactingwithfiles/csv/ReadCsvFile.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,10 +26,16 @@ public class ReadCsvFile {
 
             fileLines.forEach(System.out::println);
 
-            // the second parameter of write method must contain iterable of Strings: (Iterable<T extends Charsequence>)
-            List<String> persons = fileLines.stream()
+            // the second parameter of write method must contain iterable of Strings: (Iterable<T extends Charsequence>) this is why se convert List of Persons to the list of Strings to be able to use it in Files.write method.s
+            List<String> persons = new ArrayList<>();
+            //in case that the first line (or first few lines is header (in case of more lines use limit(x) function to retrieve first few lines
+            persons.add(Files.lines(Paths.get("./jprog2-nio2/src/main/resources/test-files/persons.csv"))
+                    .findFirst()
+                    .get());
+
+            persons.addAll(fileLines.stream()
                     .map(Person::toString)
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toList()));
             Files.write(Paths.get("./jprog2-nio2/src/main/resources/test-files/persons-modified-write.csv"), persons, Charset.forName("CP1252"));
         } catch (IOException io) {
             io.printStackTrace();
